21378a17b0ea31b2c1f213e760034078f4a91557
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2025-12-07 18:24:44 +0100

    tested azure function app locally

--- function_app.py
+++ function_app.py
@@ -1,25 +1,10 @@
 import azure.functions as func
-import logging
+import api
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="azure_function_http_trigger")
-def azure_function_http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+@app.route(route="{*route}", methods=["GET","POST","PUT","DELETE","OPTIONS"])
+async def fastapi_proxy(
+    req: func.HttpRequest, context: func.Context
+) -> func.HttpResponse:
+    return await func.AsgiMiddleware(api.app).handle_async(req, context)
